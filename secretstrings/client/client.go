33d3e153c3c50104d933f48c4e4e8453abf68691
Azalea <azaleanurjannah@gmail.com>
2025-10-16 12:53:01 +0100

    update

--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -1,16 +1,23 @@
 package main
+
 import (
-//	"net/rpc"
 	"flag"
-//	"bufio"
-//	"os"
-//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 	"fmt"
+	"net/rpc"
+
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 )
 
-func main(){
-	server := flag.String("server","127.0.0.1:8030","IP:port string to connect to as server")
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
 	flag.Parse()
 	fmt.Println("Server: ", *server)
-	//TODO: connect to the RPC server and send the request(s)
+	client, _ := rpc.DialHTTP("tcp", *server)
+	defer client.Close()
+
+	request := stubs.Request{Message: "Hello"}
+	response := new(stubs.Response)
+	client.Call(stubs.ReverseHandler, request, response)
+	fmt.Println("Response: ", response.Message)
+
 }
